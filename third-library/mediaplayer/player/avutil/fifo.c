c364de6e732d759644948f91ce3940ace2462f18
wangdy <wangdy@mail.jj.cn>
2020-11-19 12:45:03 +0800

    modify file prefix

--- third-library/mediaplayer/player/avutil/fifo.c
+++ third-library/mediaplayer/player/avutil/fifo.c
@@ -3,100 +3,100 @@
  * Copyright (c) 2000, 2001, 2002 Fabrice Bellard
  * Copyright (c) 2006 Roman Shaposhnik
  *
- * This file is part of Ijkplayer.
+ * This file is part of player.
  *
- * Ijkplayer is free software; you can redistribute it and/or
+ * player is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * Ijkplayer is distributed in the hope that it will be useful,
+ * player is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Ijkplayer; if not, write to the Free Software
+ * License along with player; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "ijkfifo.h"
-#include "ijkutils.h"
+#include "fifo.h"
+#include "utils.h"
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
 
-static IjkFifoBuffer *fifo_alloc_common(void *buffer, size_t size)
+static FifoBuffer *fifo_alloc_common(void *buffer, size_t size)
 {
-    IjkFifoBuffer *f;
+    FifoBuffer *f;
     if (!buffer)
         return NULL;
-    f = calloc(1, sizeof(IjkFifoBuffer));
+    f = calloc(1, sizeof(FifoBuffer));
     if (!f) {
         free(buffer);
         return NULL;
     }
     f->buffer = buffer;
     f->end    = f->buffer + size;
-    ijk_av_fifo_reset(f);
+    av_fifo_reset(f);
     return f;
 }
 
-IjkFifoBuffer *ijk_av_fifo_alloc(unsigned int size)
+FifoBuffer *av_fifo_alloc(unsigned int size)
 {
     void *buffer = malloc(size);
     return fifo_alloc_common(buffer, size);
 }
 
-IjkFifoBuffer *ijk_av_fifo_alloc_array(size_t nmemb, size_t size)
+FifoBuffer *av_fifo_alloc_array(size_t nmemb, size_t size)
 {
     void *buffer = calloc(nmemb, size);
     return fifo_alloc_common(buffer, nmemb * size);
 }
 
-void ijk_av_fifo_free(IjkFifoBuffer *f)
+void av_fifo_free(FifoBuffer *f)
 {
     if (f) {
-        ijk_av_freep(&f->buffer);
+        av_freep(&f->buffer);
         free(f);
     }
 }
 
-void ijk_av_fifo_freep(IjkFifoBuffer **f)
+void av_fifo_freep(FifoBuffer **f)
 {
     if (f) {
-        ijk_av_fifo_free(*f);
+        av_fifo_free(*f);
         *f = NULL;
     }
 }
 
-void ijk_av_fifo_reset(IjkFifoBuffer *f)
+void av_fifo_reset(FifoBuffer *f)
 {
     f->wptr = f->rptr = f->buffer;
     f->wndx = f->rndx = 0;
 }
 
-int ijk_av_fifo_size(const IjkFifoBuffer *f)
+int av_fifo_size(const FifoBuffer *f)
 {
     return (uint32_t)(f->wndx - f->rndx);
 }
 
-int ijk_av_fifo_space(const IjkFifoBuffer *f)
+int av_fifo_space(const FifoBuffer *f)
 {
-    return f->end - f->buffer - ijk_av_fifo_size(f);
+    return f->end - f->buffer - av_fifo_size(f);
 }
 
-int ijk_av_fifo_realloc2(IjkFifoBuffer *f, unsigned int new_size)
+int av_fifo_realloc2(FifoBuffer *f, unsigned int new_size)
 {
     unsigned int old_size = f->end - f->buffer;
 
     if (old_size < new_size) {
-        int len          = ijk_av_fifo_size(f);
-        IjkFifoBuffer *f2 = ijk_av_fifo_alloc(new_size);
+        int len          = av_fifo_size(f);
+        FifoBuffer *f2 = av_fifo_alloc(new_size);
 
         if (!f2)
             return -1;
-        ijk_av_fifo_generic_read(f, f2->buffer, len, NULL);
+        av_fifo_generic_read(f, f2->buffer, len, NULL);
         f2->wptr += len;
         f2->wndx += len;
         free(f->buffer);
@@ -106,22 +106,22 @@ int ijk_av_fifo_realloc2(IjkFifoBuffer *f, unsigned int new_size)
     return 0;
 }
 
-int ijk_av_fifo_grow(IjkFifoBuffer *f, unsigned int size)
+int av_fifo_grow(FifoBuffer *f, unsigned int size)
 {
     unsigned int old_size = f->end - f->buffer;
-    if(size + (unsigned)ijk_av_fifo_size(f) < size)
+    if(size + (unsigned)fifo_size(f) < size)
         return -1;
 
-    size += ijk_av_fifo_size(f);
+    size += fifo_size(f);
 
     if (old_size < size)
-        return ijk_av_fifo_realloc2(f, FFMAX(size, 2*old_size));
+        return fifo_realloc2(f, FFMAX(size, 2*old_size));
     return 0;
 }
 
 /* src must NOT be const as it can be a context for func that may need
  * updating (like a pointer or byte counter) */
-int ijk_av_fifo_generic_write(IjkFifoBuffer *f, void *src, int size,
+int av_fifo_generic_write(FifoBuffer *f, void *src, int size,
                           int (*func)(void *, void *, int))
 {
     int total = size;
@@ -150,7 +150,7 @@ int ijk_av_fifo_generic_write(IjkFifoBuffer *f, void *src, int size,
     return total - size;
 }
 
-int ijk_av_fifo_generic_peek_at(IjkFifoBuffer *f, void *dest, int offset, int buf_size, void (*func)(void*, void*, int))
+int av_fifo_generic_peek_at(FifoBuffer *f, void *dest, int offset, int buf_size, void (*func)(void*, void*, int))
 {
     uint8_t *rptr = f->rptr;
 
@@ -188,10 +188,11 @@ int ijk_av_fifo_generic_peek_at(IjkFifoBuffer *f, void *dest, int offset, int bu
     return 0;
 }
 
-int ijk_av_fifo_generic_peek(IjkFifoBuffer *f, void *dest, int buf_size,
+int av_fifo_generic_peek(FifoBuffer *f, void *dest, int buf_size,
                          void (*func)(void *, void *, int))
 {
-// Read memory barrier needed for SMP here in theory
+    
+    // Read memory barrier needed for SMP here in theory
     uint8_t *rptr = f->rptr;
 
     do {
@@ -202,7 +203,8 @@ int ijk_av_fifo_generic_peek(IjkFifoBuffer *f, void *dest, int buf_size,
             memcpy(dest, rptr, len);
             dest = (uint8_t *)dest + len;
         }
-// memory barrier needed for SMP here in theory
+        
+        // memory barrier needed for SMP here in theory
         rptr += len;
         if (rptr >= f->end)
             rptr -= f->end - f->buffer;
@@ -212,10 +214,11 @@ int ijk_av_fifo_generic_peek(IjkFifoBuffer *f, void *dest, int buf_size,
     return 0;
 }
 
-int ijk_av_fifo_generic_read(IjkFifoBuffer *f, void *dest, int buf_size,
+int av_fifo_generic_read(FifoBuffer *f, void *dest, int buf_size,
                          void (*func)(void *, void *, int))
 {
-// Read memory barrier needed for SMP here in theory
+    
+    // Read memory barrier needed for SMP here in theory
     do {
         int len = FFMIN(f->end - f->rptr, buf_size);
         if (func)
@@ -224,17 +227,18 @@ int ijk_av_fifo_generic_read(IjkFifoBuffer *f, void *dest, int buf_size,
             memcpy(dest, f->rptr, len);
             dest = (uint8_t *)dest + len;
         }
-// memory barrier needed for SMP here in theory
-        ijk_av_fifo_drain(f, len);
+        
+        // memory barrier needed for SMP here in theory
+        av_fifo_drain(f, len);
         buf_size -= len;
     } while (buf_size > 0);
     return 0;
 }
 
 /** Discard data from the FIFO. */
-void ijk_av_fifo_drain(IjkFifoBuffer *f, int size)
+void av_fifo_drain(FifoBuffer *f, int size)
 {
-    assert(ijk_av_fifo_size(f) >= size);
+    assert(av_fifo_size(f) >= size);
     f->rptr += size;
     if (f->rptr >= f->end)
         f->rptr -= f->end - f->buffer;
--- third-library/mediaplayer/player/avutil/fifo.h
+++ third-library/mediaplayer/player/avutil/fifo.h
@@ -96,25 +96,25 @@ int av_fifo_generic_peek_at(FifoBuffer *f, void *dest, int offset, int buf_size,
 /**
  * Feed data from an FifoBuffer to a user-supplied callback.
  * Similar as av_fifo_gereric_read but without discarding data.
- * @param f IjkFifoBuffer to read from
+ * @param f FifoBuffer to read from
  * @param buf_size number of bytes to read
  * @param func generic read function
  * @param dest data destination
  */
-int ijk_av_fifo_generic_peek(IjkFifoBuffer *f, void *dest, int buf_size, void (*func)(void*, void*, int));
+int av_fifo_generic_peek(FifoBuffer *f, void *dest, int buf_size, void (*func)(void*, void*, int));
 
 /**
- * Feed data from an IjkFifoBuffer to a user-supplied callback.
- * @param f IjkFifoBuffer to read from
+ * Feed data from an FifoBuffer to a user-supplied callback.
+ * @param f FifoBuffer to read from
  * @param buf_size number of bytes to read
  * @param func generic read function
  * @param dest data destination
  */
-int ijk_av_fifo_generic_read(IjkFifoBuffer *f, void *dest, int buf_size, void (*func)(void*, void*, int));
+int av_fifo_generic_read(FifoBuffer *f, void *dest, int buf_size, void (*func)(void*, void*, int));
 
 /**
- * Feed data from a user-supplied callback to an IjkFifoBuffer.
- * @param f IjkFifoBuffer to write to
+ * Feed data from a user-supplied callback to an FifoBuffer.
+ * @param f FifoBuffer to write to
  * @param src data source; non-const since it may be used as a
  * modifiable context by the function defined in func
  * @param size number of bytes to write
@@ -125,47 +125,47 @@ int ijk_av_fifo_generic_read(IjkFifoBuffer *f, void *dest, int buf_size, void (*
  * If func is NULL, src is interpreted as a simple byte array for source data.
  * @return the number of bytes written to the FIFO
  */
-int ijk_av_fifo_generic_write(IjkFifoBuffer *f, void *src, int size, int (*func)(void*, void*, int));
+int av_fifo_generic_write(FifoBuffer *f, void *src, int size, int (*func)(void*, void*, int));
 
 /**
- * Resize an IjkFifoBuffer.
+ * Resize an FifoBuffer.
  * In case of reallocation failure, the old FIFO is kept unchanged.
  *
- * @param f IjkFifoBuffer to resize
- * @param size new IjkFifoBuffer size in bytes
+ * @param f FifoBuffer to resize
+ * @param size new FifoBuffer size in bytes
  * @return <0 for failure, >=0 otherwise
  */
-int ijk_av_fifo_realloc2(IjkFifoBuffer *f, unsigned int size);
+int av_fifo_realloc2(FifoBuffer *f, unsigned int size);
 
 /**
- * Enlarge an IjkFifoBuffer.
+ * Enlarge an FifoBuffer.
  * In case of reallocation failure, the old FIFO is kept unchanged.
  * The new fifo size may be larger than the requested size.
  *
- * @param f IjkFifoBuffer to resize
- * @param additional_space the amount of space in bytes to allocate in addition to ijk_av_fifo_size()
+ * @param f FifoBuffer to resize
+ * @param additional_space the amount of space in bytes to allocate in addition to av_fifo_size()
  * @return <0 for failure, >=0 otherwise
  */
-int ijk_av_fifo_grow(IjkFifoBuffer *f, unsigned int additional_space);
+int av_fifo_grow(FifoBuffer *f, unsigned int additional_space);
 
 /**
- * Read and discard the specified amount of data from an IjkFifoBuffer.
- * @param f IjkFifoBuffer to read from
+ * Read and discard the specified amount of data from an FifoBuffer.
+ * @param f FifoBuffer to read from
  * @param size amount of data to read in bytes
  */
-void ijk_av_fifo_drain(IjkFifoBuffer *f, int size);
+void av_fifo_drain(FifoBuffer *f, int size);
 
 /**
  * Return a pointer to the data stored in a FIFO buffer at a certain offset.
  * The FIFO buffer is not modified.
  *
- * @param f    IjkFifoBuffer to peek at, f must be non-NULL
+ * @param f    FifoBuffer to peek at, f must be non-NULL
  * @param offs an offset in bytes, its absolute value must be less
  *             than the used buffer size or the returned pointer will
  *             point outside to the buffer data.
- *             The used buffer size can be checked with ijk_av_fifo_size().
+ *             The used buffer size can be checked with av_fifo_size().
  */
-static inline uint8_t *ijk_av_fifo_peek2(const IjkFifoBuffer *f, int offs)
+static inline uint8_t *av_fifo_peek2(const FifoBuffer *f, int offs)
 {
     uint8_t *ptr = f->rptr + offs;
     if (ptr >= f->end)
@@ -175,4 +175,4 @@ static inline uint8_t *ijk_av_fifo_peek2(const IjkFifoBuffer *f, int offs)
     return ptr;
 }
 
-#endif /* IJKUTIL_IJKFIFO_H */
+#endif
