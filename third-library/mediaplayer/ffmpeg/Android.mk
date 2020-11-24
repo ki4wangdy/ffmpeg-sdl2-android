0f2c532ba9fca3d674b963ff7485c5d548d5845f
wangdy <wangdy@mail.jj.cn>
2020-11-24 11:27:02 +0800

    add sdl library

---
+++ third-library/mediaplayer/ffmpeg/Android.mk
@@ -0,0 +1,13 @@
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ffmpeg
+
+LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/armeabi-v7a/*.a
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/*
+
+include $(PREBUILT_STATIC_LIBRARY)
+
---
+++ third-library/mediaplayer/ffmpeg/readme.txt
@@ -0,0 +1,5 @@
+
+
+参考ffmpeg-sdl2/jni/ffmpeg目录下的编译流程
+
+build_android_armv7.sh
