9762db4427cf4ddc706da63076c590d52f307c62
wangdy <wangdy@mail.jj.cn>
2020-11-24 10:44:52 +0800

    add x264 build system

---
+++ third-library/mediaplayer/x264/Android.mk
@@ -0,0 +1,11 @@
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ffmpeg
+LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/armeabi-v7a/libx264.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(PREBUILT_STATIC_LIBRARY)
+
---
+++ third-library/mediaplayer/x264/readme.txt
@@ -0,0 +1,5 @@
+
+
+参考ffmpeg-sdl2/jni/x264_android目录下的编译流程
+
+build_android_arm.sh
